9011451595a20019039dcc612fa606ad9b630664
zhuohui-xhh <email>
2020-06-29 18:26:55 +0800

    0620作业重写

--- 0620卓辉面向对象作业02/viewtodo/game/myEvent.js
+++ 0620卓辉面向对象作业02/viewtodo/game/myEvent.js
@@ -21,10 +21,18 @@ export default class MyEvent{
     // 作业： 移除事件方法补全
     removeEvent(eventName,fn){
         // 补全方法；
-        if(typeof this.handler[eventName] === 'undefined'){
-            this.handler[eventName] = [];
+        if(typeof this.handler[eventName] === 'undefined'){//如果当前事件不存在，直接return
+            return;
         }
-        this.handler[eventName].push(fn);
+        //这个事件列表是个数组，如果存在就遍历这个数组
+        for (let i = 0; i < this.handler[eventName].length; i++) {
+            if (this.handler[eventName][i] === fn) {
+                // 把方法删除；
+                this.handler[eventName].splice(i, 1);
+                break;
+            }
+        }
+        
     }
 
 
